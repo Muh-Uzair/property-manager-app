d515ab11d7f9b0903f91cb9d9b96e081b2f70709
Muh-Uzair <muhammaduzair1062001@gmail.com>
2024-08-02 15:16:08 +0500

    All 3 types of routing

--- src/App.jsx
+++ src/App.jsx
@@ -8,51 +8,59 @@
 
 import {
   createBrowserRouter,
+  createRoutesFromElements,
   Navigate,
+  Route,
   RouterProvider,
 } from "react-router-dom";
 
 import HomePG from "./Pages/HomePG";
 import ExpensesPG from "./Pages/ExpensesPG";
 import AdmissionsPG from "./Pages/AdmissionsPG";
-
 import AppLayout from "./ui/AppLayout";
 import ErrorPG from "./Pages/ErrorPG";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <AppLayout />,
-    errorElement: <ErrorPG />,
-    children: [
-      {
-        index: true,
-        element: <Navigate replace to="/home" />,
-        errorElement: <ErrorPG />,
-      },
-      {
-        path: "/home",
-        element: <HomePG />,
-        errorElement: <ErrorPG />,
-      },
-      {
-        path: "/expenses",
-        element: <ExpensesPG />,
-        errorElement: <ErrorPG />,
-      },
-      {
-        path: "/admissions",
-        element: <AdmissionsPG />,
-        errorElement: <ErrorPG />,
-      },
-      {
-        path: "*", // Catch-all for unknown URLs
-        element: <ErrorPG />,
-        errorElement: <ErrorPG />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <AppLayout />,
+//     errorElement: <ErrorPG />,
+//     children: [
+//       {
+//         index: true,
+//         element: <Navigate replace to="/home" />,
+//       },
+//       {
+//         path: "home",
+//         element: <HomePG />,
+//       },
+//       {
+//         path: "expenses",
+//         element: <ExpensesPG />,
+//       },
+//       {
+//         path: "admissions",
+//         element: <AdmissionsPG />,
+//       },
+//       {
+//         path: "*", // Catch-all for unknown URLs
+//         element: <ErrorPG />,
+//       },
+//     ],
+//   },
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<AppLayout />} errorElement={<ErrorPG />}>
+      <Route index element={<Navigate replace to="home" />} />
+      <Route path="home" element={<HomePG />} />
+      <Route path="expenses" element={<ExpensesPG />} />
+      <Route path="admissions" element={<AdmissionsPG />} />
+      <Route path="*" element={<ErrorPG />} />
+    </Route>,
+  ),
+);
 
 // COMPONENT START///////////////////////////////////////////////
 export default function App() {
