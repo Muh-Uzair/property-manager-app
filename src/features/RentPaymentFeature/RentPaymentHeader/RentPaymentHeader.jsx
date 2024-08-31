5ee2ebbf43067ccfa474bbc74eae8c500e3d1c51
Muh-Uzair <muhammaduzair1062001@gmail.com>
2024-08-31 11:37:43 +0500

    rentPayment header responsive

--- src/features/RentPaymentFeature/RentPaymentHeader/RentPaymentHeader.jsx
+++ src/features/RentPaymentFeature/RentPaymentHeader/RentPaymentHeader.jsx
@@ -1,7 +1,9 @@
+import { IoSearch } from "react-icons/io5";
+
 import Heading from "../../../ui/Heading";
 
-const commonStylesSearchFilter =
-  "py-[3px] rounded-full border-brand-color-400/60 font-semibold text-brand-color-500  border focus:outline-none focus:ring-[1px] focus:ring-brand-color-400";
+const stylesSearchFilter =
+  "bg-brand-color-300/40 border border-brand-color-400 rounded-full pl-[10px] uppercase h-[25px] text-[13px] font-semibold text-brand-color-700 focus:outline-none focus:ring-[1px] focus:border-none focus:ring-brand-color-700";
 
 // COMPONENT START
 export default function RentPaymentHeader() {
@@ -11,20 +13,39 @@ export default function RentPaymentHeader() {
 
   // JSX
   return (
-    <header className="bg-bran grid grid-cols-[auto_1fr_300px_120px] items-center gap-[10px] border-y-[3px] border-brand-color-400/60 px-[20px]">
-      <Heading type="primary">Rent Payment : Flat</Heading>
+    <header>
+      {/* heading for phone smallTab largeTab */}
+      <div className="largeScreen:hidden">
+        <Heading type="primary">Rent Payment : Flat</Heading>
+      </div>
 
-      <input
-        className={`col-start-3 px-[15px] ${commonStylesSearchFilter} bg-brand-color-300/20 placeholder-brand-color-500/50`}
-        placeholder="Search Property"
-      />
-      <select
-        className={`${commonStylesSearchFilter} bg-brand-color-300/50 px-[10px]`}
-      >
-        <option>option 1</option>
-        <option>option 2</option>
-        <option>option 3</option>
-      </select>
+      {/* input and filter */}
+      <div className="grid h-[50px] grid-cols-[auto_1fr_auto] items-center border-y-[3px] border-brand-color-400 bg-brand-color-200/30 px-[8px] largeScreen:grid-cols-[auto_1fr_auto_auto] largeScreen:gap-[20px]">
+        {/* heading for large screens */}
+        <div className="hidden largeScreen:block">
+          <Heading type="primary">Rent Payment : Flat</Heading>
+        </div>
+
+        {/* search functionality */}
+        <div className="relative flex items-center largeScreen:col-start-3">
+          <input
+            className={`${stylesSearchFilter} placeholder:text-brand-color-500/50 smallTab:w-[220px]`}
+            placeholder="Search Property"
+          />
+          <span className="absolute right-[8px] text-brand-color-700 active:text-brand-color-400">
+            <IoSearch />
+          </span>
+        </div>
+
+        {/* filter functionality */}
+        <div className="col-start-3 largeScreen:col-start-4">
+          <select className={`${stylesSearchFilter} smallTab:w-[100px]`}>
+            <option>option1</option>
+            <option>option1</option>
+            <option>option1</option>
+          </select>
+        </div>
+      </div>
     </header>
   );
   // JSX
