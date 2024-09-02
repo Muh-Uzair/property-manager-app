d7893587fdfa3dbdef288301cd7e1ee9eca21619
Muh-Uzair <muhammaduzair1062001@gmail.com>
2024-09-02 11:21:24 +0500

    rentPayment body responsive for all screens

--- src/features/RentPaymentFeature/RentPaymentBody/RentPaymentBody.jsx
+++ src/features/RentPaymentFeature/RentPaymentBody/RentPaymentBody.jsx
@@ -22,10 +22,15 @@ export default function RentPaymentBody() {
 
   // FUNCTION // to set the screen width every time the the screen width changes
   useEffect(() => {
-    window.addEventListener("resize", () => {
+    // actual function that updates the screen width
+    function updateScreenWidth() {
       const newScreenWidth = window.innerWidth;
       setScreenWidth(newScreenWidth);
-    });
+      setScreenHeight(window.innerHeight);
+    }
+
+    // event listening for the screen width
+    window.addEventListener("resize", updateScreenWidth);
   }, [screenWidth]);
 
   // FUNCTION // screen width updates , update the screen height as well ;
