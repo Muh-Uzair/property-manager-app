69c0ef5f7ab5daaefb5f271d4971bc601f3a6f4f
Muh-Uzair <muhammaduzair1062001@gmail.com>
2024-09-07 09:17:13 +0500

    screenHeigh from custom hook

---
+++ src/features/RentPaymentFeature/RentPaymentBody/useGetScreenHeight.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+export function useGetScreenHeight() {
+  const [screenHeight, setScreenHeight] = useState(window.innerHeight);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  // FUNCTION // to set the screen width every time the the screen width changes
+  useEffect(() => {
+    // actual function that updates the screen width
+    function updateScreenWidth() {
+      const newScreenWidth = window.innerWidth;
+      setScreenWidth(newScreenWidth);
+      setScreenHeight(window.innerHeight);
+    }
+
+    // event listening for the screen width
+    window.addEventListener("resize", updateScreenWidth);
+  }, [screenWidth]);
+
+  // FUNCTION // screen width updates , update the screen height as well ;
+  useEffect(() => {
+    function updateScreenHeight() {
+      setScreenHeight(window.innerHeight);
+    }
+    if (screenHeight !== window.innerHeight) {
+      updateScreenHeight();
+    } else {
+      return;
+    }
+  }, [screenWidth, screenHeight]);
+
+  return screenHeight;
+}
