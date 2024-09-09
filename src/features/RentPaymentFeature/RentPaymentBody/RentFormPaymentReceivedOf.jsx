87aa15e5bb36c511088802c823b1335fc795d559
Muh-Uzair <muhammaduzair1062001@gmail.com>
2024-09-09 21:35:49 +0500

    making checkbox controlled v2

--- src/features/RentPaymentFeature/RentPaymentBody/RentFormPaymentReceivedOf.jsx
+++ src/features/RentPaymentFeature/RentPaymentBody/RentFormPaymentReceivedOf.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import FormPortion from "../../../ui/FormPortion";
 import FormItem from "../../../ui/FormItem";
 import { getDueMonths, getLastPaidMonth } from "../../../utils/helpers";
+import { useState } from "react";
 
 // COMPONENT START
 export default function RentFormPaymentReceivedOf({
@@ -10,6 +11,7 @@ export default function RentFormPaymentReceivedOf({
   register,
 }) {
   // VARIABLES
+  const [amountReceived] = useState(10);
 
   // JSX
   return (
@@ -45,6 +47,8 @@ export default function RentFormPaymentReceivedOf({
           id={"rfAmountReceived"}
           name={"rfAmountReceived"}
           register={register}
+          controlled={true}
+          controllerStVar={amountReceived}
         />
       </FormPortion>
     </>
