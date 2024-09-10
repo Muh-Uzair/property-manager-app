a492769394065afaa4f9353c8e529340e74f08a6
Muh-Uzair <muhammaduzair1062001@gmail.com>
2024-09-10 18:42:42 +0500

    amount received functionality almost done

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ import PropertyEdit from "./features/ShowPropertyDetails/PropertyEdit";
 import RentPaymentPG from "./Pages/RentPaymentPG";
 import DummyPG1 from "./Pages/DummyPG1";
 
-// rentPaymentForm refactored
+// amount received functionality almost done
 
 const router = createBrowserRouter(
   createRoutesFromElements(
