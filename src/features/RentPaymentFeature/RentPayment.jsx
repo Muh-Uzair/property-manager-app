cf00ab28b23918554de47e40f28ad2c9a0108728
Muh-Uzair <muhammaduzair1062001@gmail.com>
2024-09-16 10:58:52 +0500

    before correcting the context

--- src/features/RentPaymentFeature/RentPayment.jsx
+++ src/features/RentPaymentFeature/RentPayment.jsx
@@ -1,16 +1,24 @@
+import { createContext, useState } from "react";
 import RentPaymentBody from "./RentPaymentBody/RentPaymentBody";
 import RentPaymentHeader from "./RentPaymentHeader/RentPaymentHeader";
+
+export const RentPaymentContext = createContext();
 
 // COMPONENT START
 export default function RentPayment() {
   // VARIABLES
+  const [isSearchingProperty, setIsSearchingProperty] = useState(false);
 
   // JSX
   return (
-    <div className={`grid h-[100%] w-[100%] grid-rows-[auto_1fr] gap-[10px]`}>
-      <RentPaymentHeader />
-      <RentPaymentBody />
-    </div>
+    <RentPaymentContext.Provider
+      value={{ isSearchingProperty, setIsSearchingProperty }}
+    >
+      <div className={`grid h-[100%] w-[100%] grid-rows-[auto_1fr] gap-[10px]`}>
+        <RentPaymentHeader />
+        <RentPaymentBody />
+      </div>
+    </RentPaymentContext.Provider>
   );
   // JSX
 }
--- src/features/RentPaymentFeature/RentPaymentHeader/RentPaymentHeader.jsx
+++ src/features/RentPaymentFeature/RentPaymentHeader/RentPaymentHeader.jsx
@@ -3,7 +3,7 @@ import { IoSearch } from "react-icons/io5";
 import Heading from "../../../ui/Heading";
 import PropertyChangeBtns from "../../../ui/PropertyChangeBtns";
 import { useGetPropertyType } from "../../../hooks/useGetPropertyType";
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const stylesSearchFilter =
   "bg-brand-color-300/40 border border-brand-color-400 rounded-full pl-[10px] uppercase h-[25px] text-[13px] font-semibold text-brand-color-700 focus:outline-none focus:ring-[1px] focus:border-none focus:ring-brand-color-700";
@@ -12,14 +12,15 @@ const stylesSearchFilter =
 export default function RentPaymentHeader() {
   // VARIABLES
   const propertyType = useGetPropertyType();
-  const [inputSearch, setInputSearch] = useState(
-    propertyType?.slice(0, -1) ?? "flat",
-  );
+  const { register, handleSubmit } = useForm();
 
   // FUNCTION
-  function updateInputSearch(e) {
-    console.log(e.target.value);
-    setInputSearch(e.target.value);
+  function handleSubmitProperty(data) {
+    console.log(data);
+  }
+  // FUNCTION
+  function handleSubmitPropertyError(errors) {
+    console.log(errors);
   }
 
   // JSX
@@ -50,20 +51,29 @@ export default function RentPaymentHeader() {
         </div>
 
         {/* search functionality */}
-        <div className="relative flex items-center largeScreen:col-start-3">
+        <form
+          className="relative flex items-center largeScreen:col-start-3"
+          onSubmit={handleSubmit(
+            handleSubmitProperty,
+            handleSubmitPropertyError,
+          )}
+        >
           <input
             type="text"
             className={`${stylesSearchFilter} placeholder:text-brand-color-500/50 smallTab:w-[220px]`}
             placeholder="Search Property"
             name={"propertySearchInput"}
-            value={inputSearch}
-            onChange={(e) => updateInputSearch(e)}
             maxLength={8}
+            value={`${propertyType.slice(0, -1)} `}
+            {...register("propertySearchInput", { required: true })}
           />
-          <span className="absolute right-[8px] text-brand-color-700 active:text-brand-color-400">
+          <button
+            type="submit"
+            className="absolute right-[8px] text-brand-color-700 active:text-brand-color-400"
+          >
             <IoSearch />
-          </span>
-        </div>
+          </button>
+        </form>
 
         {/* filter functionality */}
         <div className="col-start-3 largeScreen:col-start-4">
