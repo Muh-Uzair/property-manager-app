2dff2a640c5130218f4b905fa43947fbbfa607c5
Muh-Uzair <muhammaduzair1062001@gmail.com>
2024-12-03 21:58:07 +0500

    error oundary removed

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/index.css";
-import { ErrorBoundary } from "react-error-boundary";
-// import ErrorFallback from "./ui/ErrorFallback.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ErrorBoundary FallbackComponent={<span>hello</span>}>
-      <App />
-    </ErrorBoundary>
+    <App />
   </React.StrictMode>,
 );
